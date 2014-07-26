4cbee924255788b201b5295c453b0b91ac3ef506
Wolf <wolf@mykolab.com>
2014-07-26 01:01:55 -0500

    Better keccakf_mul implementation.

--- wildkeccak.c
+++ wildkeccak.c
@@ -20,60 +20,60 @@ enum {
 };
 
 #define KK_MIXIN_SIZE 24
-#define ROTL64(x, y) (((x) << (y)) | ((x) >> (64 - (y))))
+__attribute__((const)) static inline uint64_t rotl641(uint64_t x) { return((x << 1) | (x >> 63)); }
+__attribute__((const)) static inline uint64_t rotl64_1(uint64_t x, uint64_t y) { return((x << y) | (x >> (64 - y))); }
+__attribute__((const)) static inline uint64_t rotl64_2(uint64_t x, uint64_t y) { return(rotl64_1((x >> 32) | (x << 32), y)); }
+__attribute__((const)) static inline uint64_t bitselect(uint64_t a, uint64_t b, uint64_t c) { return(a ^ (c & (b ^ a))); }
 
-static __always_inline void keccakf_mul(uint64_t s[25])
+static __always_inline void keccakf_mul(uint64_t *s)
 {
-    uint64_t t[5], v, w;
-
-    t[0] = s[0] ^ s[5] ^ s[10] * s[15] * s[20] ^
-        ROTL64(s[2] ^ s[7] ^ s[12] * s[17] * s[22], 1);
-    t[1] = s[1] ^ s[6] ^ s[11] * s[16] * s[21] ^
-        ROTL64(s[3] ^ s[8] ^ s[13] * s[18] * s[23], 1);
-    t[2] = s[2] ^ s[7] ^ s[12] * s[17] * s[22] ^
-        ROTL64(s[4] ^ s[9] ^ s[14] * s[19] * s[24], 1);
-    t[3] = s[3] ^ s[8] ^ s[13] * s[18] * s[23] ^
-        ROTL64(s[0] ^ s[5] ^ s[10] * s[15] * s[20], 1);
-    t[4] = s[4] ^ s[9] ^ s[14] * s[19] * s[24] ^
-        ROTL64(s[1] ^ s[6] ^ s[11] * s[16] * s[21], 1);
-
-    v = s[ 1] ^ t[0];
-    s[0] ^= t[4];
-
-    s[ 1] = ROTL64(s[ 6] ^ t[0], 44);
-    s[ 6] = ROTL64(s[ 9] ^ t[3], 20);
-    s[ 9] = ROTL64(s[22] ^ t[1], 61);
-    s[22] = ROTL64(s[14] ^ t[3], 39);
-    s[14] = ROTL64(s[20] ^ t[4], 18);
-    s[20] = ROTL64(s[ 2] ^ t[1], 62);
-    s[ 2] = ROTL64(s[12] ^ t[1], 43);
-    s[12] = ROTL64(s[13] ^ t[2], 25);
-    s[13] = ROTL64(s[19] ^ t[3],  8);
-    s[19] = ROTL64(s[23] ^ t[2], 56);
-    s[23] = ROTL64(s[15] ^ t[4], 41);
-    s[15] = ROTL64(s[ 4] ^ t[3], 27);
-    s[ 4] = ROTL64(s[24] ^ t[3], 14);
-    s[24] = ROTL64(s[21] ^ t[0],  2);
-    s[21] = ROTL64(s[ 8] ^ t[2], 55);
-    s[ 8] = ROTL64(s[16] ^ t[0], 45);
-    s[16] = ROTL64(s[ 5] ^ t[4], 36);
-    s[ 5] = ROTL64(s[ 3] ^ t[2], 28);
-    s[ 3] = ROTL64(s[18] ^ t[2], 21);
-    s[18] = ROTL64(s[17] ^ t[1], 15);
-    s[17] = ROTL64(s[11] ^ t[0], 10);
-    s[11] = ROTL64(s[ 7] ^ t[1],  6);
-    s[ 7] = ROTL64(s[10] ^ t[4],  3);
-    s[10] = ROTL64(    v,         1);
-
-    /* chi: a[i,j] ^= ~b[i,j+1] & b[i,j+2] */
-    v = s[ 0]; w = s[ 1]; s[ 0] ^= (~w) & s[ 2]; s[ 1] ^= (~s[ 2]) & s[ 3]; s[ 2] ^= (~s[ 3]) & s[ 4]; s[ 3] ^= (~s[ 4]) & v; s[ 4] ^= (~v) & w;
-    v = s[ 5]; w = s[ 6]; s[ 5] ^= (~w) & s[ 7]; s[ 6] ^= (~s[ 7]) & s[ 8]; s[ 7] ^= (~s[ 8]) & s[ 9]; s[ 8] ^= (~s[ 9]) & v; s[ 9] ^= (~v) & w;
-    v = s[10]; w = s[11]; s[10] ^= (~w) & s[12]; s[11] ^= (~s[12]) & s[13]; s[12] ^= (~s[13]) & s[14]; s[13] ^= (~s[14]) & v; s[14] ^= (~v) & w;
-    v = s[15]; w = s[16]; s[15] ^= (~w) & s[17]; s[16] ^= (~s[17]) & s[18]; s[17] ^= (~s[18]) & s[19]; s[18] ^= (~s[19]) & v; s[19] ^= (~v) & w;
-    v = s[20]; w = s[21]; s[20] ^= (~w) & s[22]; s[21] ^= (~s[22]) & s[23]; s[22] ^= (~s[23]) & s[24]; s[23] ^= (~s[24]) & v; s[24] ^= (~v) & w;
-
-    /* iota: a[0,0] ^= round constant (first round) */
-    s[0] ^= 1;
+    uint64_t bc[5], t[5];
+    uint64_t tmp1, tmp2;
+	int i;
+	
+	for(i = 0; i < 5; i++)
+		t[i] = s[i + 0] ^ s[i + 5] ^ s[i + 10] * s[i + 15] * s[i + 20];
+	
+	bc[0] = t[0] ^ rotl641(t[2]);
+	bc[1] = t[1] ^ rotl641(t[3]);
+	bc[2] = t[2] ^ rotl641(t[4]);
+	bc[3] = t[3] ^ rotl641(t[0]);
+	bc[4] = t[4] ^ rotl641(t[1]);
+	
+	tmp1 = s[1] ^ bc[0];
+	
+	s[0] ^= bc[4];
+	s[1] = rotl64_2(s[6] ^ bc[0], 12);
+	s[6] = rotl64_1(s[9] ^ bc[3], 20);
+	s[9] = rotl64_2(s[22] ^ bc[1], 29);
+	s[22] = rotl64_2(s[14] ^ bc[3], 7);
+	s[14] = rotl64_1(s[20] ^ bc[4], 18);
+	s[20] = rotl64_2(s[2] ^ bc[1], 30);
+	s[2] = rotl64_2(s[12] ^ bc[1], 11);
+	s[12] = rotl64_1(s[13] ^ bc[2], 25);
+	s[13] = rotl64_1(s[19] ^ bc[3], 8);
+	s[19] = rotl64_2(s[23] ^ bc[2], 24);
+	s[23] = rotl64_2(s[15] ^ bc[4], 9);
+	s[15] = rotl64_1(s[4] ^ bc[3], 27);
+	s[4] = rotl64_1(s[24] ^ bc[3], 14);
+	s[24] = rotl64_1(s[21] ^ bc[0], 2);
+	s[21] = rotl64_2(s[8] ^ bc[2], 23);
+	s[8] = rotl64_2(s[16] ^ bc[0], 13);
+	s[16] = rotl64_2(s[5] ^ bc[4], 4);
+	s[5] = rotl64_1(s[3] ^ bc[2], 28);
+	s[3] = rotl64_1(s[18] ^ bc[2], 21);
+	s[18] = rotl64_1(s[17] ^ bc[1], 15);
+	s[17] = rotl64_1(s[11] ^ bc[0], 10);
+	s[11] = rotl64_1(s[7] ^ bc[1], 6);
+	s[7] = rotl64_1(s[10] ^ bc[4], 3);
+	s[10] = rotl64_1(tmp1, 1);
+	
+	tmp1 = s[0]; tmp2 = s[1]; s[0] = bitselect(s[0] ^ s[2], s[0], s[1]); s[1] = bitselect(s[1] ^ s[3], s[1], s[2]); s[2] = bitselect(s[2] ^ s[4], s[2], s[3]); s[3] = bitselect(s[3] ^ tmp1, s[3], s[4]); s[4] = bitselect(s[4] ^ tmp2, s[4], tmp1);
+	tmp1 = s[5]; tmp2 = s[6]; s[5] = bitselect(s[5] ^ s[7], s[5], s[6]); s[6] = bitselect(s[6] ^ s[8], s[6], s[7]); s[7] = bitselect(s[7] ^ s[9], s[7], s[8]); s[8] = bitselect(s[8] ^ tmp1, s[8], s[9]); s[9] = bitselect(s[9] ^ tmp2, s[9], tmp1);
+	tmp1 = s[10]; tmp2 = s[11]; s[10] = bitselect(s[10] ^ s[12], s[10], s[11]); s[11] = bitselect(s[11] ^ s[13], s[11], s[12]); s[12] = bitselect(s[12] ^ s[14], s[12], s[13]); s[13] = bitselect(s[13] ^ tmp1, s[13], s[14]); s[14] = bitselect(s[14] ^ tmp2, s[14], tmp1);
+	tmp1 = s[15]; tmp2 = s[16]; s[15] = bitselect(s[15] ^ s[17], s[15], s[16]); s[16] = bitselect(s[16] ^ s[18], s[16], s[17]); s[17] = bitselect(s[17] ^ s[19], s[17], s[18]); s[18] = bitselect(s[18] ^ tmp1, s[18], s[19]); s[19] = bitselect(s[19] ^ tmp2, s[19], tmp1);
+	tmp1 = s[20]; tmp2 = s[21]; s[20] = bitselect(s[20] ^ s[22], s[20], s[21]); s[21] = bitselect(s[21] ^ s[23], s[21], s[22]); s[22] = bitselect(s[22] ^ s[24], s[22], s[23]); s[23] = bitselect(s[23] ^ tmp1, s[23], s[24]); s[24] = bitselect(s[24] ^ tmp2, s[24], tmp1);
+	s[0] ^= 0x0000000000000001ULL;
 }
 
 static __always_inline void wildkeccak(uint64_t* st, const uint64_t* pscr, uint64_t scr_size, struct reciprocal_value64 recip)
